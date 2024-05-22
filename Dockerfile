47fb8af81fdbe3c8ad7be44a0a93699935bf6f85
Drew Harris <harrisd@smu.edu>
2024-05-21 20:25:28 -0500

    Fix redirects!

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:20-slim AS base
+ENV PNPM_HOME="/pnpm"
+ENV PATH="$PNPM_HOME:$PATH"
+RUN corepack enable
+
+FROM base AS build
+COPY . /usr/src/app
+WORKDIR /usr/src/app
+RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --frozen-lockfile
+
+RUN pnpm run build
+
+EXPOSE 3000
+
+CMD ["node", "dist/server.js"]
--- src/internal/serverPageHandler.tsx
+++ src/internal/serverPageHandler.tsx
@@ -5,38 +5,37 @@ import { drewsRenderToStream } from "./streamer";
 
 export const handlePage = async (c: Context) => {
   c.header("Content-Type", "text/html; charset=utf-8");
-  return stream(c, async (stream) => {
-    try {
-      const { app, router } = await entry.render(c.req.raw);
-      // TODO: Getting closer
-      // if (router.history.location.href != c.req.raw.url) {
-      //   console.log("Redirecting to", router.history.location.href);
-      //   // c.redirect(router.history.location.href);
-      //   // return;
-      // }
-      const { stream: ssrxStream, statusCode } = await drewsRenderToStream({
-        app: () => app,
-        req: c.req.raw,
-        injectToStream: [
-          {
-            async emitBeforeStreamChunk() {
-              const injectorPromises = router.injectedHtml.map((d) =>
-                typeof d === "function" ? d() : d,
-              );
-              const injectors = await Promise.all(injectorPromises);
-              router.injectedHtml = [];
-              return injectors.join("");
-            },
+  try {
+    const { app, router } = await entry.render(c.req.raw);
+    // TODO: Getting closer
+    if (router.state.redirect && router.state.redirect?.to) {
+      console.log("Redirecting to", router.state.redirect?.to);
+      return c.redirect(router.state.redirect?.to);
+    }
+    const { stream: ssrxStream, statusCode } = await drewsRenderToStream({
+      app: () => app,
+      req: c.req.raw,
+      injectToStream: [
+        {
+          async emitBeforeStreamChunk() {
+            const injectorPromises = router.injectedHtml.map((d) =>
+              typeof d === "function" ? d() : d,
+            );
+            const injectors = await Promise.all(injectorPromises);
+            router.injectedHtml = [];
+            return injectors.join("");
           },
-        ],
-      });
+        },
+      ],
+    });
 
-      let status = statusCode();
-      if (router.hasNotFoundMatch() && status !== 500) status = 404;
+    let status = statusCode();
+    if (router.hasNotFoundMatch() && status !== 500) status = 404;
 
-      // Set the headers directly on the context
-      c.header("Content-Type", "text/html; charset=utf-8");
+    // Set the headers directly on the context
+    c.header("Content-Type", "text/html; charset=utf-8");
 
+    return stream(c, async (stream) => {
       stream.onAbort(() => {
         if (!ssrxStream.locked) {
           ssrxStream.cancel();
@@ -57,9 +56,9 @@ export const handlePage = async (c: Context) => {
           }
         });
       }
-    } catch (err) {
-      console.error("Server-side rendering failed:", err);
-      throw err; // Rethrow to let Hono handle the error
-    }
-  });
+    });
+  } catch (err) {
+    console.error("Server-side rendering failed:", err);
+    throw err; // Rethrow to let Hono handle the error
+  }
 };
